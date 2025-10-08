4206646389887304024a5e599cdec25b4fe5c40e
Sir Panda <lmmfs.1998@gmail.com>
2025-10-08 15:25:14 +0100

    Initial commit

---
+++ Core/Source/Core/Renderer/Shader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+#include <filesystem>
+
+namespace Renderer {
+
+	uint32_t CreateComputeShader(const std::filesystem::path& path);
+	uint32_t ReloadComputeShader(uint32_t shaderHandle, const std::filesystem::path& path);
+
+	uint32_t CreateGraphicsShader(const std::filesystem::path& vertexPath, const std::filesystem::path& fragmentPath);
+	uint32_t ReloadGraphicsShader(uint32_t shaderHandle, const std::filesystem::path& vertexPath, const std::filesystem::path& fragmentPath);
+
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Splitter game engine
+
+## Build
+
+Generate project files/build using CMake. I like to make a directory called `build` at the root and then run
+```
+cmake ..
+```
+from that directory. This will generate relevant project files.
+
+## Notes
+ OpenGL (and GLFW).
+
+## License
